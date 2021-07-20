c7210c9ab458b9e338acaad5d20af939473d360d
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2021-07-20 21:37:59 +0100

    Added laser turret project

---
+++ Laser_Turret/Laser_Turret.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo x;
+Servo y;
+
+void setup() {
+  x.attach(D0);
+  y.attach(D1);
+
+  x.write(90);
+  y.write(90);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
