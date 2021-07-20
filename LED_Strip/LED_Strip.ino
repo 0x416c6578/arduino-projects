d16362d6b2f5541a4d6fd3cbe5fe6dfdc0401b2c
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2021-07-20 19:25:05 +0100

    Reverted formatting on LED_Strip

--- LED_Strip/LED_Strip.ino
+++ LED_Strip/LED_Strip.ino
@@ -16,96 +16,96 @@ uint8_t rCurr = 0,
         bCurr = 0;
 
 void setup() {
-    pinMode(PIN_RED, OUTPUT);
-    pinMode(PIN_GREEN, OUTPUT);
-    pinMode(PIN_BLUE, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
 
-    //There are no externall pullups/pulldowns so internal pullups must be enabled
-    pinMode(12, INPUT_PULLUP);
-    pinMode(11, INPUT_PULLUP);
-    pinMode(10, INPUT_PULLUP);
-    pinMode(9, INPUT_PULLUP);
+  //There are no externall pullups/pulldowns so internal pullups must be enabled
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
 }
 
 void loop() {
-    if (getSwitches() == 0b0000) {
-        /*
-        colMode = random() % 6;
-        if (colMode == 0)
-          fadeTo(random() % 156 + 100, random() % 100, random() % 100);
-        else if (colMode == 1)
-          fadeTo(random() % 100, random() % 156 + 100, random() % 100);
-        else if (colMode == 2)
-          fadeTo(random() % 100, random() % 100, random() % 156 + 100);
-        else if (colMode == 3)
-          fadeTo(random() % 156 + 100, random() % 156 + 100, random() % 100);
-        else if (colMode == 4)
-          fadeTo(random() % 100, random() % 156 + 100, random() % 156 + 100);
-          */
-    } else if (getSwitches() == 0b0001) {
-        writeColour(COL_RED);
-    } else if (getSwitches() == 0b0010) {
-        writeColour(COL_GREEN);
-    } else if (getSwitches() == 0b0011) {
-        writeColour(COL_BLUE);
-    } else if (getSwitches() == 0b0100) {
-        if (colMode >= 3)
-            colMode = 0;
-        if (colMode == 0)
-            fadeTo(COL_RED);
-        if (colMode == 1)
-            fadeTo(COL_GREEN);
-        if (colMode == 2)
-            fadeTo(COL_BLUE);
-        colMode++;
-        delay(1000);
-    }
+  if (getSwitches() == 0b0000) {
+    /*
+    colMode = random() % 6;
+    if (colMode == 0)
+      fadeTo(random() % 156 + 100, random() % 100, random() % 100);
+    else if (colMode == 1)
+      fadeTo(random() % 100, random() % 156 + 100, random() % 100);
+    else if (colMode == 2)
+      fadeTo(random() % 100, random() % 100, random() % 156 + 100);
+    else if (colMode == 3)
+      fadeTo(random() % 156 + 100, random() % 156 + 100, random() % 100);
+    else if (colMode == 4)
+      fadeTo(random() % 100, random() % 156 + 100, random() % 156 + 100);
+      */
+  } else if (getSwitches() == 0b0001) {
+    writeColour(COL_RED);
+  } else if (getSwitches() == 0b0010) {
+    writeColour(COL_GREEN);
+  } else if (getSwitches() == 0b0011) {
+    writeColour(COL_BLUE);
+  } else if (getSwitches() == 0b0100) {
+    if (colMode >= 3)
+      colMode = 0;
+    if (colMode == 0)
+      fadeTo(COL_RED);
+    if (colMode == 1)
+      fadeTo(COL_GREEN);
+    if (colMode == 2)
+      fadeTo(COL_BLUE);
+    colMode++;
+    delay(1000);
+  }
 }
 
 void writeColour(uint32_t col) {
-    analogWrite(PIN_RED,   HEX_TO_RED(col));
-    analogWrite(PIN_GREEN, HEX_TO_GREEN(col));
-    analogWrite(PIN_BLUE,  HEX_TO_BLUE(col));
+  analogWrite(PIN_RED,   HEX_TO_RED(col));
+  analogWrite(PIN_GREEN, HEX_TO_GREEN(col));
+  analogWrite(PIN_BLUE,  HEX_TO_BLUE(col));
 }
 
 void fadeTo(uint32_t col) {
-    uint8_t red = HEX_TO_RED(col);
-    uint8_t green = HEX_TO_GREEN(col);
-    uint8_t blue = HEX_TO_BLUE(col);
+  uint8_t red = HEX_TO_RED(col);
+  uint8_t green = HEX_TO_GREEN(col);
+  uint8_t blue = HEX_TO_BLUE(col);
 
-    //Get the number of steps that need to be traversed to reach the target colour (max of red, green or blue)
-    uint8_t maxStep = max(max(abs(red - rCurr), abs(green - gCurr)), abs(blue - bCurr));
-    uint8_t del = COL_CHANGE_TIME / maxStep;
+  //Get the number of steps that need to be traversed to reach the target colour (max of red, green or blue)
+  uint8_t maxStep = max(max(abs(red - rCurr), abs(green - gCurr)), abs(blue - bCurr));
+  uint8_t del = COL_CHANGE_TIME / maxStep;
 
-    //Loop while we haven't yet reached the target colours
-    while (rCurr != red || bCurr != blue || gCurr != green) {
-        //For each colour, move a step closer to the target
-        if (rCurr < red)
-            rCurr++;
-        else if (rCurr > red)
-            rCurr--;
+  //Loop while we haven't yet reached the target colours
+  while (rCurr != red || bCurr != blue || gCurr != green) {
+    //For each colour, move a step closer to the target
+    if (rCurr < red)
+      rCurr++;
+    else if (rCurr > red)
+      rCurr--;
 
-        if (gCurr < green)
-            gCurr++;
-        else if (gCurr > green)
-            gCurr--;
+    if (gCurr < green)
+      gCurr++;
+    else if (gCurr > green)
+      gCurr--;
 
-        if (bCurr < blue)
-            bCurr++;
-        else if (bCurr > blue)
-            bCurr--;
+    if (bCurr < blue)
+      bCurr++;
+    else if (bCurr > blue)
+      bCurr--;
 
-        //Write the new colour to the strip
-        analogWrite(PIN_RED, rCurr);
-        analogWrite(PIN_GREEN, gCurr);
-        analogWrite(PIN_BLUE, bCurr);
-        delay(del);
-    }
+    //Write the new colour to the strip
+    analogWrite(PIN_RED, rCurr);
+    analogWrite(PIN_GREEN, gCurr);
+    analogWrite(PIN_BLUE, bCurr);
+    delay(del);
+  }
 }
 
 uint8_t getSwitches() {
-    return (digitalRead(12))
-           | (digitalRead(11) << 1)
-           | (digitalRead(10) << 2)
-           | (digitalRead(9) << 3);
+  return (digitalRead(12))
+         | (digitalRead(11) << 1)
+         | (digitalRead(10) << 2)
+         | (digitalRead(9) << 3);
 }
