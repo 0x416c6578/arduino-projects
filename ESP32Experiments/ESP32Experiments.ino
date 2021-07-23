9accc4c6d339f364d9862216a584daa8d9563a62
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2021-07-23 16:18:33 +0100

    ESP32 now gets temperature over HTTP from PI3B

--- ESP32Experiments/ESP32Experiments.ino
+++ ESP32Experiments/ESP32Experiments.ino
@@ -1,6 +1,11 @@
 #include <TFT_eSPI.h>
 #include <SPI.h>
+#include "HTTPClient.h"
 #include "WiFi.h"
+
+#define HTTP_PORT 8080
+#define HTTP_METHOD "GET"
+#define HOSTNAME "pi-3b"
 
 TFT_eSPI tft = TFT_eSPI(135,240);
 char buff[512];
@@ -15,15 +20,27 @@ void setup() {
   WiFi.setHostname("ESP32");
   WiFi.mode(WIFI_STA);
   WiFi.begin("BT-Home", "3MpDHpFMHNgHcD");
+
+  tft.setCursor(0,0);
+  tft.println("Connecting...");
+  tft.setCursor(0,0);
   while (WiFi.status() != WL_CONNECTED) {
-    tft.drawString("Connecting", 0, 0);
-    delay(1000);
+    delay(500);
   }
   tft.fillScreen(TFT_WHITE);
-  tft.drawString(WiFi.localIP().toString(), 0, 0);
+  tft.println(WiFi.localIP().toString());
 
+  HTTPClient client;
 
+  tft.setTextSize(3);
+  client.begin(HOSTNAME, HTTP_PORT);
+  int code = client.GET();
+  String payload = client.getString();
+  // tft.printf("%04d\n", code);
+  tft.println();
+  tft.println(payload);
 }
+
 
 void loop() {
 }
