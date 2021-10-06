eab64a42479affc3b99937fa305d480508437902
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2021-10-06 23:59:43 +0100

    Added code to fork sensor stuff

--- Fork_Sensor_Emulator/Fork_Sensor_Emulator.ino
+++ Fork_Sensor_Emulator/Fork_Sensor_Emulator.ino
@@ -1,9 +1,18 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Rough timings based on calculations in info page
+  for(int i = 0; i < 14; i++) {
+    digitalWrite(13, HIGH);
+    delay(33);
+    digitalWrite(13, LOW);
+    delay(33);
+  }
+  digitalWrite(13, HIGH);
+  delay(16);
+  digitalWrite(13, LOW);
+  delay(49);
 }
